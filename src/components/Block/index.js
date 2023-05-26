fdf521edbc0b1f9fcf022cf02261a4cf6c5a474e
luan_khong_ngu <luan.pitech@gmail.com>
2023-05-26 20:47:22 +0700

    UI Bottomtab

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -1,16 +1,16 @@
-import {theme} from '@theme';
-import {getSize} from '@utils/responsive';
-import {isNumber} from 'lodash';
-import React from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { theme } from "@theme";
+import { getSize } from "@utils/responsive";
+import { isNumber } from "lodash";
+import React from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import {
   handleMargin,
   handlePadding,
   handleRound,
   handleSquare,
-} from '../shared';
-import styles from './styles';
+} from "../shared";
+import styles from "./styles";
 
 const Block = ({
   flex,
@@ -80,16 +80,16 @@ const Block = ({
 
   const blockStyles = [
     isPaddingIos && {
-      paddingBottom: Platform.OS === 'ios' ? insets.bottom : getSize.m(20),
+      paddingBottom: Platform.OS === "ios" ? insets.bottom : getSize.m(20),
     },
     flex && styles.block,
     flexShrink && styles.flexShrink,
     flexGrow && styles.flexGrow,
-    !flex && {flex: 0},
-    maxWidth && {maxWidth},
-    maxHeight && {maxHeight},
-    width && {width: width},
-    height && {height: height},
+    !flex && { flex: 0 },
+    maxWidth && { maxWidth },
+    maxHeight && { maxHeight },
+    width && { width: width },
+    height && { height: height },
     row && styles.row,
     column && styles.column,
     shadow && {
@@ -97,58 +97,58 @@ const Block = ({
       shadowColor: shadowColor ? theme.colors[shadowColor] : theme.colors.black,
       elevation: elevation || 3,
     },
-    wrap && {flexWrap: 'wrap'},
+    wrap && { flexWrap: "wrap" },
     backgroundColor && {
       backgroundColor: theme.colors[backgroundColor] || backgroundColor,
     },
-    padding && {...handlePadding(getSize.m(padding))},
-    margin && {...handleMargin(getSize.m(margin))},
+    padding && { ...handlePadding(getSize.m(padding)) },
+    margin && { ...handleMargin(getSize.m(margin)) },
     alignStart && styles.alignStart,
     alignCenter && styles.alignCenter,
     alignEnd && styles.alignEnd,
     justifyCenter && styles.justifyCenter,
     justifyStart && styles.justifyStart,
     justifyEnd && styles.justifyEnd,
-    space && {justifyContent: `space-${space}`},
-    paddingTop && {paddingTop: getSize.m(paddingTop)},
-    paddingRight && {paddingRight: getSize.m(paddingRight)},
-    paddingBottom && {paddingBottom: getSize.m(paddingBottom)},
-    paddingLeft && {paddingLeft: getSize.m(paddingLeft)},
-    marginBottom && {marginBottom: getSize.m(marginBottom)},
-    marginTop && {marginTop: getSize.m(marginTop)},
-    marginRight && {marginRight: getSize.m(marginRight)},
-    marginLeft && {marginLeft: getSize.m(marginLeft)},
-    paddingHorizontal && {paddingHorizontal: getSize.m(paddingHorizontal)},
-    paddingVertical && {paddingVertical: getSize.m(paddingVertical)},
-    marginHorizontal && {marginHorizontal: getSize.m(marginHorizontal)},
-    marginVertical && {marginVertical: getSize.m(marginVertical)},
-    radius && {borderRadius: getSize.s(radius)},
-    borderWidth && {borderWidth: borderWidth},
-    square && {...handleSquare(getSize.s(square))},
-    round && {...handleRound(getSize.s(round))},
-    isNumber(opacity) && {opacity: opacity},
+    space && { justifyContent: `space-${space}` },
+    paddingTop && { paddingTop: getSize.m(paddingTop) },
+    paddingRight && { paddingRight: getSize.m(paddingRight) },
+    paddingBottom && { paddingBottom: getSize.m(paddingBottom) },
+    paddingLeft && { paddingLeft: getSize.m(paddingLeft) },
+    marginBottom && { marginBottom: getSize.m(marginBottom) },
+    marginTop && { marginTop: getSize.m(marginTop) },
+    marginRight && { marginRight: getSize.m(marginRight) },
+    marginLeft && { marginLeft: getSize.m(marginLeft) },
+    paddingHorizontal && { paddingHorizontal: getSize.m(paddingHorizontal) },
+    paddingVertical && { paddingVertical: getSize.m(paddingVertical) },
+    marginHorizontal && { marginHorizontal: getSize.m(marginHorizontal) },
+    marginVertical && { marginVertical: getSize.m(marginVertical) },
+    radius && { borderRadius: getSize.s(radius) },
+    borderWidth && { borderWidth: borderWidth },
+    square && { ...handleSquare(getSize.s(square)) },
+    round && { ...handleRound(getSize.s(round)) },
+    isNumber(opacity) && { opacity: opacity },
     borderColor && {
       borderColor:
         theme.colors[borderColor] || theme.colors.border || borderColor,
     },
-    relative && {position: 'relative'},
-    absolute && {position: 'absolute'},
-    isNumber(top) && {top: getSize.v(top)},
-    isNumber(left) && {left: getSize.s(left)},
-    isNumber(right) && {right: getSize.s(right)},
-    isNumber(bottom) && {bottom: getSize.v(bottom)},
-    overflow && {overflow},
-    alignSelf && {alignSelf},
-    borderTopWidth && {borderTopWidth},
-    borderRightWidth && {borderRightWidth},
-    borderBottomWidth && {borderBottomWidth},
-    borderLeftWidth && {borderLeftWidth},
-    zIndex && {zIndex},
-    borderTopLeftRadius && {borderTopLeftRadius},
-    borderTopRightRadius && {borderTopRightRadius},
-    borderBottomRightRadius && {borderBottomRightRadius},
-    borderBottomLeftRadius && {borderBottomLeftRadius},
-    {...StyleSheet.flatten(style)},
+    relative && { position: "relative" },
+    absolute && { position: "absolute" },
+    isNumber(top) && { top: getSize.v(top) },
+    isNumber(left) && { left: getSize.s(left) },
+    isNumber(right) && { right: getSize.s(right) },
+    isNumber(bottom) && { bottom: getSize.v(bottom) },
+    overflow && { overflow },
+    alignSelf && { alignSelf },
+    borderTopWidth && { borderTopWidth },
+    borderRightWidth && { borderRightWidth },
+    borderBottomWidth && { borderBottomWidth },
+    borderLeftWidth && { borderLeftWidth },
+    zIndex && { zIndex },
+    borderTopLeftRadius && { borderTopLeftRadius },
+    borderTopRightRadius && { borderTopRightRadius },
+    borderBottomRightRadius && { borderBottomRightRadius },
+    borderBottomLeftRadius && { borderBottomLeftRadius },
+    { ...StyleSheet.flatten(style) },
   ];
 
   return (
